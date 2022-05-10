cbe6a21f26edbb2d659faad87e9454ccbf28a001
Gri-ffin <gameshunter359@gmail.com>
2022-05-10 19:52:34 +0100

    base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className='flex justify-center items-center flex-col h-screen space-y-3'>
+      <h1 className='text-3xl md:text-4xl'>Feeling Hungry?</h1>
+      <p className='text-xl md:text-2xl'>Get a random meal by clicking below</p>
+      <button className='uppercase bg-blue-500 text-white text-xs px-7 py-3 rounded-md font-bold'>
+        Get Meal 🍔
+      </button>
+    </div>
+  );
 }
 
 export default App;
