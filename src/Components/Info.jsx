22c64d6b85ff043206a797974ec681c625c47c1e
Gri-ffin <gameshunter359@gmail.com>
2022-05-10 20:20:16 +0100

    support arrays

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,7 +1,10 @@
 const MealInfo = ({ info, text }) => {
   return (
     <h6 className='font-bold'>
-      {text}: <span className='font-medium'>{info}</span>
+      {text}:{' '}
+      <span className='font-medium'>
+        {typeof info === 'object' ? info?.join(', ') : info}
+      </span>
     </h6>
   );
 };
